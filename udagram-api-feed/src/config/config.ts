177c473433c121deb82acb91fea0b8640cb784e9
Giovani Machado <giovani.machado@gmail.com>
2021-02-27 21:17:11 -0300

    refactor: backends are separated into

---
+++ udagram-api-feed/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': "udagram_postgres",
+  'password': "udagram_postgres",
+  'database': "postgres",
+  'host': "cdr.cqqgmzj7mghz.us-east-1.rds.amazonaws.com",
+  'dialect': 'postgres',
+  'aws_region': "us-east-1",
+  'aws_profile': "default",
+  'aws_media_bucket': "udagram-712062568472-dev",
+  'url': "http://localhost:8100",
+  'jwt': {
+    'secret': "hello",
+  },
+};
